0e1fa8b245333134f61ff9e0b4fc301374cbe130
ariuka-09 <ariuntuguldur3@gmail.com>
2025-10-21 19:26:03 +0800

    tried to add multiple students

--- index.js
+++ index.js
@@ -13,53 +13,63 @@ const port = process.env.PORT
 
  app.use(bodyParser.json())
 
- const studentArray = []
-let printingArray = []
- app.listen(port, ()=>{  
-    console.log(chalk.magenta(`server is serving! http://localhost${port}`))
+ let studentArray = []
 
-    
+ app.listen(port, ()=>{
+    console.log(chalk.magenta(`server is serving! http://localhost${port}`))
 })
 
-app.get('/student-detail', async (request, response)=>{
+app.get('/student-detail',  (request, response)=>{
+    const {number} = request.params;
+    console.log(number, "number");
     
+    if(number){
+        studentArray = studentArray.filter((student)=>
+            student.phone == number)
+    }
 
     const {gender, age} = request.query;
-    if(gender){
-        const filteredStudents = studentArray.filter(student => 
+    if(gender || age){
+        studentArray = studentArray.filter(student => 
             student.gender?.toLowerCase() === gender.toLowerCase() && student.age >= age
-          );
-          return response.send(filteredStudents).end()
+        );
     }
     response.send(studentArray)
+    // response.send(...studentArray)
+    //why isnt this spreading properly?
+    //answer it can only return 1 array
 })
 
-
 app.post('/student', (request, response)=>{
-    const thisNameExists = studentArray.filter((student)=>{
-        if(student.phone===request.body.phone)return true;
-    })
-    if(thisNameExists.length==0){
-studentArray.push(request.body);
+    let thisNameExists;
+    let studentIndex = [];
+    console.log(request.body.length, "the length");
+    
+    for(let i = 0 ; i < request.body.length; i++){
+         thisNameExists = studentArray.filter((student)=>{
+            const thisNameExists = student.filter(student=>{if(student.phone==request.body[i].phone)return true})
+                if(thisNameExists.length!==0){
+                    return true;
+                }
+        })
+    }
+
+    if(thisNameExists.length == 0){
+studentArray.push( request.body );
 return response.status(200).send(studentArray).end()
     }else{
-
         response.status(409).send({message: 'student already exists'}) 
-       
     }
 })
 
 app.delete('/student', (request, response)=>{
-
     console.log(request.body)
  for( let i = 0 ; i < studentArray.length; i++){
     if(request.body.phone==studentArray[i].phone){
         studentArray.splice(i, 1)
     }
  }
-
 response.send("deletion successful")
-
 })
 
 app.put('/student', (request, response)=>{
@@ -69,30 +79,5 @@ app.put('/student', (request, response)=>{
             studentArray[i]= request.body.update
         }
     }
-
 response.send("updated successfully")
-
 })
-
-const students = {
-    studentsCount:21,
-    names:[
-        "Amaraa", "Urantogos", "Baasanbayar", "Tuvshin", "Temuulen", "Badmaa", "Bayarjavhlan", "Suhee", "Baagii", "Enhzul", "Temuujin", "Tserenpuntsag", "Bayarmaa", "Ariuka", "Galsan", "Subeedei", "Sarangoo", "Zoloo", "Bolormaa", "Husel", "Barsaa",
-    ]
-}
-
-const studentDetail = {
-    age:61,
-    name: "Super",
-    lastName: "Man",
-}
-
-
-
-
-
-
-
-
-
-// getDataByGoogle();
